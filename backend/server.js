8fc07f506a659963e5dff6ad98515743a7ecab36
Raj Aryan <rajpatil484950@gmail.com>
2026-04-09 19:07:58 +0530

    Pass Key Change

--- backend/server.js
+++ backend/server.js
@@ -20,9 +20,31 @@ app.use(express.static("public"));
 app.use("/uploads", express.static("uploads"));
 
 /* CORS */
+
+// const allowedOrigins = [
+//   "http://localhost:5173",
+//   "https://flamius.vercel.app"
+// ];
+// app.use(cors({
+//   // origin: "https://flamius.vercel.app",
+//    origin: true,
+//   credentials: true,
+// }));
+
+const allowedOrigins = [
+  "http://localhost:5173",
+  "https://flamius.vercel.app"
+];
+
 app.use(cors({
-  origin: "https://flamius.vercel.app",
-  credentials: true,
+  origin: function (origin, callback) {
+    if (!origin || allowedOrigins.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error("Not allowed by CORS"));
+    }
+  },
+  credentials: true
 }));
 
 /* MIDDLEWARE */
